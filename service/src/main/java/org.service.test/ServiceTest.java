b18df7ddd7cde3c852ad2086524860461d4ad3d8
adrass <adrien.daolena@gmail.com>
2013-04-14 19:09:27 +0200

    test

---
+++ service/src/main/java/org.service.test/ServiceTest.java
@@ -0,0 +1,14 @@
+package org.service.test;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: adrass
+ * Date: 13/04/13
+ * Time: 16:48
+ * To change this template use File | Settings | File Templates.
+ */
+public class ServiceTest {
+    public void sayHello(){
+        System.out.println("hello from bundle");
+    }
+}
