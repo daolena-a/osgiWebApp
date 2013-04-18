dac7870bbfc508dd4b5a296a320909eb52532d89
adrass <adrien.daolena@gmail.com>
2013-04-18 21:41:18 +0200

    fin du poc

--- service/src/main/java/org.service.test/ServiceTest.java
+++ service/src/main/java/org.service.test/ServiceTest.java
@@ -1,11 +1,7 @@
 package org.service.test;
 
 /**
- * Created with IntelliJ IDEA.
- * User: adrass
- * Date: 13/04/13
- * Time: 16:48
- * To change this template use File | Settings | File Templates.
+ *just a test
  */
 public class ServiceTest {
     public void sayHello(){
--- service/src/main/java/org/osgi/app/OtherService.java
+++ service/src/main/java/org/osgi/app/OtherService.java
@@ -1,11 +1,7 @@
 package org.osgi.app;
 
 /**
- * Created with IntelliJ IDEA.
- * User: adrass
- * Date: 13/04/13
- * Time: 19:29
- * To change this template use File | Settings | File Templates.
+ * Just a service
  */
 public class OtherService {
     public void sayHello(){
